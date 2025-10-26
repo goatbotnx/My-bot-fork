6901d3d687d967bc6286ade72f2d2f137851eef1
goatbotnx <onlygoatbot.nx@gmail.com>
2025-10-26 10:49:08 +0600

    Update sendMessage.js

--- fb-chat-api/src/sendMessage.js
+++ fb-chat-api/src/sendMessage.js
@@ -16,16 +16,15 @@ var allowedProperties = {
 };
 
 module.exports = function (defaultFuncs, api, ctx) {
-	// Upload attachments function
 	function uploadAttachment(attachments, callback) {
 		var uploads = [];
 
-		// Create an array of promises for all attachments
+		// create an array of promises
 		for (var i = 0; i < attachments.length; i++) {
 			if (!utils.isReadableStream(attachments[i])) throw { error: "Attachment should be a readable stream and not " + utils.getType(attachments[i]) + "." };
 			var form = {
 				upload_1024: attachments[i],
-				voice_clip: "true" // You can add conditions for other types if needed
+				voice_clip: "true"
 			};
 
 			uploads.push(
@@ -34,12 +33,14 @@ module.exports = function (defaultFuncs, api, ctx) {
 					.then(utils.parseAndCheckLogin(ctx, defaultFuncs))
 					.then(function (resData) {
 						if (resData.error) throw resData;
+						// We have to return the data unformatted unless we want to change it
+						// back in sendMessage.
 						return resData.payload.metadata[0];
 					})
 			);
 		}
 
-		// Resolve all promises
+		// resolve all promises
 		bluebird
 			.all(uploads)
 			.then(resData => callback(null, resData))
@@ -49,7 +50,6 @@ module.exports = function (defaultFuncs, api, ctx) {
 			});
 	}
 
-	// Get URL attachment for sharing
 	function getUrl(url, callback) {
 		var form = {
 			image_height: 960,
@@ -62,7 +62,7 @@ module.exports = function (defaultFuncs, api, ctx) {
 			.then(utils.parseAndCheckLogin(ctx, defaultFuncs))
 			.then(function (resData) {
 				if (resData.error) return callback(resData);
-				if (!resData.payload) return callback({ error: "Invalid URL" });
+				if (!resData.payload) return callback({ error: "Invalid url" });
 				callback(null, resData.payload.share_data.share_params);
 			})
 			.catch(function (err) {
@@ -71,19 +71,27 @@ module.exports = function (defaultFuncs, api, ctx) {
 			});
 	}
 
-	// Send content to the thread
 	function sendContent(form, threadID, isSingleUser, messageAndOTID, callback) {
+		// There are three cases here:
+		// 1. threadID is of type array, where we're starting a new group chat with users
+		//    specified in the array.
+		// 2. User is sending a message to a specific user.
+		// 3. No additional form params and the message goes to an existing group chat.
 		if (utils.getType(threadID) === "Array") {
 			for (var i = 0; i < threadID.length; i++) form["specific_to_list[" + i + "]"] = "fbid:" + threadID[i];
 			form["specific_to_list[" + threadID.length + "]"] = "fbid:" + ctx.userID;
 			form["client_thread_id"] = "root:" + messageAndOTID;
 			log.info("sendMessage", "Sending message to multiple users: " + threadID);
-		} else {
+		}
+		else {
+			// This means that threadID is the id of a user, and the chat
+			// is a single person chat
 			if (isSingleUser) {
 				form["specific_to_list[0]"] = "fbid:" + threadID;
 				form["specific_to_list[1]"] = "fbid:" + ctx.userID;
 				form["other_user_fbid"] = threadID;
-			} else form["thread_fbid"] = threadID;
+			}
+			else form["thread_fbid"] = threadID;
 		}
 
 		if (ctx.globalOptions.pageID) {
@@ -105,18 +113,21 @@ module.exports = function (defaultFuncs, api, ctx) {
 				if (resData.error) {
 					if (resData.error === 1545012) {
 						log.warn("sendMessage", "Got error 1545012. This might mean that you're not part of the conversation " + threadID);
-					} else {
+					}
+					else {
 						log.error("sendMessage", resData);
 					}
 					return callback(resData);
 				}
 
 				var messageInfo = resData.payload.actions.reduce(function (p, v) {
-					return {
-						threadID: v.thread_fbid,
-						messageID: v.message_id,
-						timestamp: v.timestamp
-					} || p;
+					return (
+						{
+							threadID: v.thread_fbid,
+							messageID: v.message_id,
+							timestamp: v.timestamp
+						} || p
+					);
 				}, null);
 
 				return callback(null, messageInfo);
@@ -128,7 +139,17 @@ module.exports = function (defaultFuncs, api, ctx) {
 			});
 	}
 
-	// Handle message types like URL, Location, Sticker, Emoji, Attachments, Mentions
+	function send(form, threadID, messageAndOTID, callback, isGroup) {
+		// We're doing a query to this to check if the given id is the id of
+		// a user or of a group chat. The form will be different depending
+		// on that.
+		if (utils.getType(threadID) === "Array") sendContent(form, threadID, false, messageAndOTID, callback);
+		else {
+			if (utils.getType(isGroup) != "Boolean") sendContent(form, threadID, threadID.length === 15, messageAndOTID, callback);
+			else sendContent(form, threadID, !isGroup, messageAndOTID, callback);
+		}
+	}
+
 	function handleUrl(msg, form, callback, cb) {
 		if (msg.url) {
 			form["shareable_attachment[share_type]"] = "100";
@@ -137,12 +158,13 @@ module.exports = function (defaultFuncs, api, ctx) {
 				form["shareable_attachment[share_params]"] = params;
 				cb();
 			});
-		} else cb();
+		}
+		else cb();
 	}
 
 	function handleLocation(msg, form, callback, cb) {
 		if (msg.location) {
-			if (msg.location.latitude == null || msg.location.longitude == null) return callback({ error: "Location property needs both latitude and longitude." });
+			if (msg.location.latitude == null || msg.location.longitude == null) return callback({ error: "location property needs both latitude and longitude" });
 			form["location_attachment[coordinates][latitude]"] = msg.location.latitude;
 			form["location_attachment[coordinates][longitude]"] = msg.location.longitude;
 			form["location_attachment[is_current_location]"] = !!msg.location.current;
@@ -156,11 +178,11 @@ module.exports = function (defaultFuncs, api, ctx) {
 	}
 
 	function handleEmoji(msg, form, callback, cb) {
-		if (msg.emojiSize != null && msg.emoji == null) return callback({ error: "Emoji property is empty." });
+		if (msg.emojiSize != null && msg.emoji == null) return callback({ error: "emoji property is empty" });
 		if (msg.emoji) {
 			if (msg.emojiSize == null) msg.emojiSize = "medium";
-			if (msg.emojiSize != "small" && msg.emojiSize != "medium" && msg.emojiSize != "large") return callback({ error: "Invalid emojiSize." });
-			if (form["body"] != null && form["body"] != "") return callback({ error: "Body should be empty when emoji is set." });
+			if (msg.emojiSize != "small" && msg.emojiSize != "medium" && msg.emojiSize != "large") return callback({ error: "emojiSize property is invalid" });
+			if (form["body"] != null && form["body"] != "") return callback({ error: "body is not empty" });
 			form["body"] = msg.emoji;
 			form["tags[0]"] = "hot_emoji_size:" + msg.emojiSize;
 		}
@@ -176,10 +198,11 @@ module.exports = function (defaultFuncs, api, ctx) {
 			form["audio_ids"] = [];
 
 			if (utils.getType(msg.attachment) !== "Array") msg.attachment = [msg.attachment];
-			if (msg.attachment.every(e => /_id$/.test(e[0]))) {
-				msg.attachment.forEach(e => form[`${e[0]}s`].push(e[1]));
+			if (msg.attachment.every(e=>/_id$/.test(e[0]))) {
+				//console.log(msg.attachment)
+				msg.attachment.map(e=>form[`${e[0]}s`].push(e[1]));
 				return cb();
-			}
+			  }
 			uploadAttachment(msg.attachment, function (err, files) {
 				if (err) return callback(err);
 				files.forEach(function (file) {
@@ -189,7 +212,8 @@ module.exports = function (defaultFuncs, api, ctx) {
 				});
 				cb();
 			});
-		} else cb();
+		}
+		else cb();
 	}
 
 	function handleMention(msg, form, callback, cb) {
@@ -203,4 +227,103 @@ module.exports = function (defaultFuncs, api, ctx) {
 				if (mention.id == null) log.warn("handleMention", "Mention id should be non-null.");
 
 				const id = mention.id || 0;
-				const emptyChar = '\u200
+				const emptyChar = '\u200E';
+				form["body"] = emptyChar + msg.body;
+				form["profile_xmd[" + i + "][offset]"] = offset + 1;
+				form["profile_xmd[" + i + "][length]"] = tag.length;
+				form["profile_xmd[" + i + "][id]"] = id;
+				form["profile_xmd[" + i + "][type]"] = "p";
+			}
+		}
+		cb();
+	}
+
+	return function sendMessage(msg, threadID, callback, replyToMessage, isGroup) {
+		typeof isGroup == "undefined" ? isGroup = null : "";
+		if (!callback && (utils.getType(threadID) === "Function" || utils.getType(threadID) === "AsyncFunction")) return threadID({ error: "Pass a threadID as a second argument." });
+		if (!replyToMessage && utils.getType(callback) === "String") {
+			replyToMessage = callback;
+			callback = function () { };
+		}
+
+		var resolveFunc = function () { };
+		var rejectFunc = function () { };
+		var returnPromise = new Promise(function (resolve, reject) {
+			resolveFunc = resolve;
+			rejectFunc = reject;
+		});
+
+		if (!callback) {
+			callback = function (err, data) {
+				if (err) return rejectFunc(err);
+				resolveFunc(data);
+			};
+		}
+
+		var msgType = utils.getType(msg);
+		var threadIDType = utils.getType(threadID);
+		var messageIDType = utils.getType(replyToMessage);
+
+		if (msgType !== "String" && msgType !== "Object") return callback({ error: "Message should be of type string or object and not " + msgType + "." });
+
+		// Changing this to accomodate an array of users
+		if (threadIDType !== "Array" && threadIDType !== "Number" && threadIDType !== "String") return callback({ error: "ThreadID should be of type number, string, or array and not " + threadIDType + "." });
+
+		if (replyToMessage && messageIDType !== 'String') return callback({ error: "MessageID should be of type string and not " + threadIDType + "." });
+
+		if (msgType === "String") msg = { body: msg };
+		var disallowedProperties = Object.keys(msg).filter(prop => !allowedProperties[prop]);
+		if (disallowedProperties.length > 0) return callback({ error: "Dissallowed props: `" + disallowedProperties.join(", ") + "`" });
+
+		var messageAndOTID = utils.generateOfflineThreadingID();
+		// console.log(messageAndOTID)
+		var form = {
+			client: "mercury",
+			action_type: "ma-type:user-generated-message",
+			author: "fbid:" + ctx.userID,
+			timestamp: Date.now(),
+			timestamp_absolute: "Today",
+			timestamp_relative: utils.generateTimestampRelative(),
+			timestamp_time_passed: "0",
+			is_unread: false,
+			is_cleared: false,
+			is_forward: false,
+			is_filtered_content: false,
+			is_filtered_content_bh: false,
+			is_filtered_content_account: false,
+			is_filtered_content_quasar: false,
+			is_filtered_content_invalid_app: false,
+			is_spoof_warning: false,
+			source: "source:chat:web",
+			"source_tags[0]": "source:chat",
+			body: msg.body ? msg.body.toString() : "",
+			html_body: false,
+			ui_push_phase: "V3",
+			status: "0",
+			offline_threading_id: messageAndOTID,
+			message_id: messageAndOTID,
+			threading_id: utils.generateThreadingID(ctx.clientID),
+			"ephemeral_ttl_mode:": "0",
+			manual_retry_cnt: "0",
+			has_attachment: !!(msg.attachment || msg.url || msg.sticker),
+			signatureID: utils.getSignatureID(),
+			replied_to_message_id: replyToMessage
+		};
+		// console.log(form)
+
+		handleLocation(msg, form, callback, () =>
+			handleSticker(msg, form, callback, () =>
+				handleAttachment(msg, form, callback, () =>
+					handleUrl(msg, form, callback, () =>
+						handleEmoji(msg, form, callback, () =>
+							handleMention(msg, form, callback, () =>
+								send(form, threadID, messageAndOTID, callback, isGroup)
+							)
+						)
+					)
+				)
+			)
+		);
+		return returnPromise;
+	};
+};
